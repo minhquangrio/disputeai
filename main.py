d2501a5d52266b56760853809b58832754fd27d3
minhquangrio <147734098+minhquangrio@users.noreply.github.com>
2023-12-19 15:46:14 +0700

    Add files via upload
    Thêm tính năng /time ,  get thành viên

--- main.py
+++ main.py
@@ -1,54 +1,69 @@
-import random
-import re
-import requests
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import telebot
+import datetime
 
+# Mở file token.txtpy 
+with open("token.txt", "r") as f:
+    token = f.read().strip()
 
-   
+# Tạo bot
+bot = telebot.TeleBot(token)
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-#Máy tính
-async def calculate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Lấy nội dung của tin nhắn và loại bỏ lệnh '/calculate'
-    expression = update.message.text.replace('/calculate', '').strip()
-
-    try:
-        # Kiểm tra xem biểu thức có hợp lệ không bằng cách sử dụng regex
-        if not re.match(r"^[0-9+\-*/.() ]+$", expression):
-            raise ValueError("Biểu thức không hợp lệ.")
-        
-        # Thực hiện phép tính
-        result = eval(expression)
-        
-        # Trả kết quả
-        await update.message.reply_text(f"Kết quả của biểu thức '{expression}' là: {result}")
-    except Exception as e:
-        await update.message.reply_text(f"Đã xảy ra lỗi: {str(e)}")
-
-
-# Hàm để lấy trích đoạn ngẫu nhiên từ API Quotable
-def get_random_quote():
-    url = "https://api.quotable.io/random"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return f"{data['content']} - {data['author']}"
+# Xử lý tin nhắn
+@bot.message_handler(commands=['start', 'help', 'time', 'get_group_info'])
+def start(message):
+    if message.text == '/start':
+        bot.send_message(message.chat.id, 'Xin chào! Tôi là bot Telegram được tạo bằng Python.')
+    elif message.text == '/help':
+        bot.send_message(message.chat.id, 'Có thể làm gì với tôi?')
     else:
-        return "Không thể lấy trích đoạn ngẫu nhiên."
+        bot.send_message(message.chat.id, 'Bạn muốn biết thời gian hiện tại không?')
 
-# Hàm xử lý tính năng thư giãn
-async def relax(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    quote = get_random_quote()
-    await update.message.reply_text(quote)
+# Xử lý tin nhắn thời gian
+@bot.message_handler(commands=['time'])
+def time(message):
+    now = datetime.datetime.now()
+    bot.send_message(message.chat.id, 'Bây giờ là {}'.format(now.strftime('%H:%M:%S')))
+
+# Xử lý tin nhắn lấy tên và ID của nhóm
+@bot.message_handler(commands=['get_group_info'])
+def get_group_info(message):
+    # Lấy ID của nhóm
+    group_id = message.chat.id
+
+    # Lấy thông tin về nhóm
+    group = bot.get_chat(group_id)
+
+    # Gửi tin nhắn với tên và ID của nhóm
+    bot.send_message(message.chat.id, 'Tên nhóm: {}'.format(group.title))
+    bot.send_message(message.chat.id, 'ID nhóm: {}'.format(group.id))
+
+# Xử lý tin nhắn
+@bot.message_handler(commands=['get_channel_info'])
+def get_channel_info(message):
+    # Lấy ID của kênh
+    channel_id = message.chat.id
+
+    # Lấy thông tin về kênh
+    channel = bot.get_chat(channel_id)
+
+    # Gửi tin nhắn với tên và ID của kênh
+    bot.send_message(message.chat.id, 'Tên kênh: {}'.format(channel.title))
+    bot.send_message(message.chat.id, 'ID kênh: {}'.format(channel.id))
+
+# Xử lý tin nhắn lấy số lượng thành viên trong nhóm
+@bot.message_handler(commands=['get_group_members'])
+def get_group_members(message):
+    # Lấy ID của nhóm
+    group_id = message.chat.id
+
+    # Lấy số lượng thành viên trong nhóm
+    group_members_count = bot.get_chat_members_count(group_id)
+
+    # Gửi số lượng thành viên cho người dùng
+    bot.send_message(message.chat.id, 'Số lượng thành viên trong nhóm này là: {}'.format(group_members_count))
 
 
-app = ApplicationBuilder().token("6814966479:AAE7JjxF1SrnrrD1oALzA7f4JJ2pPqof0TM").build()
 
-app.add_handler(CommandHandler("hello", hello))
-app.add_handler(CommandHandler("calculate", calculate))
-app.add_handler(CommandHandler("relax", relax))
 
-app.run_polling()
+# Chạy bot
+bot.polling()
